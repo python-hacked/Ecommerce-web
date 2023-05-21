543396b1f82ed81f24f16e1dc4cdfd320d860565
python-hacked <satishchoudhary394@gmail.com>
2023-05-21 19:54:50 +0530

    add json

--- amazone/app/static/app/js/myscript.js
+++ amazone/app/static/app/js/myscript.js
@@ -22,52 +22,52 @@ $('#slider1, #slider2, #slider3').owlCarousel({
     }
 })
 
-$('.plus-cart').click(function(){
+$('.plus-cart').click(function () {
     var id = $(this).attr("pid").toString();
     var eml = this.parentNode.children[2]
     $.ajax({
         type: "GET",
         url: "/pluscart",
-        data:{
+        data: {
             prod_id: id
         },
-        success: function(data){
+        success: function (data) {
             eml.innerText = data.quantity
-            document.getElementById("amount").innerText=data.amount
-            document.getElementById("totalamount").innerText=data.totalamount
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText = data.totalamount
         }
     })
 })
 
-$('.minus-cart').click(function(){
+$('.minus-cart').click(function () {
     var id = $(this).attr("pid").toString();
     var eml = this.parentNode.children[2]
     $.ajax({
         type: "GET",
         url: "/minuscart",
-        data:{
+        data: {
             prod_id: id
         },
-        success: function(data){
+        success: function (data) {
             eml.innerText = data.quantity
-            document.getElementById("amount").innerText=data.amount
-            document.getElementById("totalamount").innerText=data.totalamount
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText = data.totalamount
         }
     })
 })
 
-$('.remove-cart').click(function(){
+$('.remove-cart').click(function () {
     var id = $(this).attr("pid").toString();
     var eml = this
     $.ajax({
         type: "GET",
         url: "/removecart",
-        data:{
+        data: {
             prod_id: id
         },
-        success: function(data){
-            document.getElementById("amount").innerText=data.amount
-            document.getElementById("totalamount").innerText=data.totalamount
+        success: function (data) {
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText = data.totalamount
             eml.parentNode.parentNode.parentNode.parentNode.remove()
         }
     })
